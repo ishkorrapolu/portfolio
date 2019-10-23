12f70b982cec72c2d5c802b0436b53dde1ff21f7
Ishwarya Reddy Korrapolu <korrapo2@uwm.edu>
2019-10-23 10:55:24 -0500

    Updates

---
+++ precache-manifest.f3bf1e6cd90948964881e5fd0d020946.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f343b4bb627ef5a877e6c293afd53565",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "9322c12dd98a4449da0d",
+    "url": "/portfolio/static/css/2.1be8104b.chunk.css"
+  },
+  {
+    "revision": "4749fc770295bebae0c3",
+    "url": "/portfolio/static/css/main.7a5f27c8.chunk.css"
+  },
+  {
+    "revision": "9322c12dd98a4449da0d",
+    "url": "/portfolio/static/js/2.5e1fd14c.chunk.js"
+  },
+  {
+    "revision": "4749fc770295bebae0c3",
+    "url": "/portfolio/static/js/main.da484da3.chunk.js"
+  },
+  {
+    "revision": "c45404ce61b3482bf4cf",
+    "url": "/portfolio/static/js/runtime-main.8a01d333.js"
+  }
+]);
